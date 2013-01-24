6c79fbc4a6e305143e766c24888af3ec713797fd
pathfinder <chm2920@gmail.com>
2013-01-24 10:56:03 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnionStatistics::Application.config.secret_token = 'c41a69658cd251188ad9e11c32b0553d7e5ebd28155c4ba267c09864b7e15757d6299dde1ad5b1dd419470761d3b1a323c254fba8e96e1d45ad8908966047b92'
